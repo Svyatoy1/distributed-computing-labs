3b19c0634e8c00500fb9fe33073a506c386573b5
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-05 18:02:47 +0000

    allocation + DummyDataInitialization function + updated main

--- lab01/README.md
+++ lab01/README.md
@@ -1,8 +1,13 @@
-## EX2 
-# Task 1 - Open the Project (Стартове повідомлення)
+# EX2 
+## Task 1 - Open the Project (Стартове повідомлення)
 **Що зроблено:** після оголошення змінних додано у main додано вивід "Serial matrix-vector multiplication program", а також додано інформацію на gitignore
 
-# Task 2 — ProcessInitialization: ввід Size із перевіркою
+## Task 2 — ProcessInitialization: ввід Size із перевіркою
 **Додано:** функцію `ProcessInitialization(double*&, double*&, double*&, int&)`.
 **Логіка:** цикл `do…while` запитує розмір доти, доки `Size > 0`. Некоректний ввід чистимо через `cin.clear()` + `cin.ignore(...)`.
 **Вивід:** показує `Chosen objects size = N`
+
+## Task 3 — Memory allocation + DummyDataInitialization
+**Що зроблено:** у `ProcessInitialization(...)` після коректного вводу `Size` додаємо `new[]` для `pMatrix` (Size×Size), `pVector` (Size), `pResult` (Size) та викликаємо `DummyDataInitialization(pMatrix, pVector, Size)`, яка заповнює: рядок i — значення i, вектор — 1.
+**Перевірка:** для Size=3 очікувано A =
+[0 0 0; 1 1 1; 2 2 2], b = [1 1 1]
--- lab01/src/SerialMV.cpp
+++ lab01/src/SerialMV.cpp
@@ -10,6 +10,16 @@ double* pVector; // Second argument - initial vector
 double* pResult; // Result vector for matrix-vector multiplication
 int Size; // Sizes of initial matrix and vector
 
+// Function for simple definition of matrix and vector elements
+void DummyDataInitialization(double* pMatrix, double* pVector, int Size) {
+    for (int i = 0; i < Size; ++i) {
+        pVector[i] = 1.0;
+        for (int j = 0; j < Size; ++j)
+            pMatrix[i * Size + j] = static_cast<double>(i);
+    }
+}
+
+// Function for memory allocation and data initialization
 void ProcessInitialization(double* &pMatrix, double* &pVector, double* &pResult, int &Size) {
     do {
         cout << "\nEnter size of the initial objects: ";
@@ -24,15 +34,39 @@ void ProcessInitialization(double* &pMatrix, double* &pVector, double* &pResult,
         if (Size <= 0)
             cout << "Size of objects must be greater than 0!\n";
     } while (Size <= 0);
+
+    //Memory allocation
+    pMatrix = new double [Size*Size];
+    pVector = new double [Size];
+    pResult = new double [Size];
+
+    DummyDataInitialization(pMatrix, pVector, Size);
+}
+
+// Function for formatted matrix output
+void PrintMatrix (double* pMatrix, int RowCount, int ColCount) {
+    int i, j; // Loop variables
+        for (i=0; i<RowCount; i++) {
+            for (j=0; j<ColCount; j++)
+            printf("%7.4f ", pMatrix[i*ColCount+j]);
+            printf("\n");
+        }
+    }
+// Function for formatted vector output
+void PrintVector (double* pVector, int Size) {
+    int i;
+    for (i=0; i<Size; i++)
+    printf("%7.4f ", pVector[i]);
+    printf("\n");
 }
 
 int main() {
-    cout << "Serial matrix-vector multiplication program\n";
-
+    // Memory allocation and data initialization
     ProcessInitialization(pMatrix, pVector, pResult, Size);
-    cout << "Press Enter to exit";
-
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // прибрати \n після числа
-    cin.get();
+    // Matrix and vector output
+    printf ("Initial Matrix: \n");
+    PrintMatrix (pMatrix, Size, Size);
+    printf ("Initial Vector: \n");
+    PrintVector (pVector, Size);
     return 0;
 }
