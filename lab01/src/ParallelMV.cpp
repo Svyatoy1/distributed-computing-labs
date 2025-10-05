68e23ec8ec6391fc69f5d33ca352e25162eaa24d
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-05 22:29:58 +0000

    Input the Initial Data

--- lab01/src/ParallelMV.cpp
+++ lab01/src/ParallelMV.cpp
@@ -7,30 +7,75 @@ using namespace std;
 
 int ProcNum, ProcRank; // Number of available processes, rank of current process
 
-void ProcessInitialization(double*& pMatrix, double*& pVector, double*& pResult, int& Size) {
+// Function for simple definition of matrix and vector elements
+void DummyDataInitialization(double* pMatrix, double* pVector, int Size) {
+    for (int i = 0; i < Size; ++i) {
+        pVector[i] = 1.0;
+        for (int j = 0; j < Size; ++j)
+            pMatrix[i * Size + j] = static_cast<double>(i);
+    }
+}
+
+// Function for formatted matrix output
+void PrintMatrix (double* pMatrix, int RowCount, int ColCount) {
+    int i, j; // Loop variables
+        for (i=0; i<RowCount; i++) {
+            for (j=0; j<ColCount; j++)
+            printf("%7.4f ", pMatrix[i*ColCount+j]);
+            printf("\n");
+        }
+    }
+    
+// Function for formatted vector output
+void PrintVector (double* pVector, int Size) {
+    int i;
+    for (i=0; i<Size; i++)
+    printf("%7.4f ", pVector[i]);
+    printf("\n");
+}
+
+void ProcessInitialization(double*& pMatrix, double*& pVector, double*& pResult, 
+    double*& pProcRows, double*& pProcResult, int& Size, int& RowNum) {
     if (ProcRank == 0) {
         do {
             cout << "\nEnter size of the matrix and vector: ";
             cin >> Size;
-
             if (Size < ProcNum)
                 cout << "Size must be greater than number of processes!\n";
             if (Size % ProcNum != 0)
                 cout << "Size must be divisible by number of processes!\n";
-
         } while (Size < ProcNum || Size % ProcNum != 0);
-
-        cout << "Chosen size = " << Size << "\n";
     }
 
     MPI_Bcast(&Size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    RowNum = Size / ProcNum;
+
+    pVector = new double[Size];
+    pResult = new double[Size];
+    pProcRows = new double[RowNum * Size];
+    pProcResult = new double[RowNum];
+
+    if (ProcRank == 0) {
+        pMatrix = new double[Size * Size];
+        DummyDataInitialization(pMatrix, pVector, Size);
+
+        cout << "\nInitial Matrix (on root process):\n";
+        PrintMatrix(pMatrix, Size, Size);
+
+        cout << "\nInitial Vector:\n";
+        PrintVector(pVector, Size);
+    }
 }
 
 int main(int argc, char* argv[]) {
     double* pMatrix; // First argument - initial matrix
     double* pVector; // Second argument - initial vector
     double* pResult; // Result vector for matrix-vector multiplication
+    double* pProcRows;
+    double* pProcResult;
     int Size; // Sizes of initial matrix and vector
+    int RowNum;
     double Start, Finish, Duration;
     
     MPI_Init(&argc, &argv); // initialization of MPI envoriment
@@ -40,9 +85,7 @@ int main(int argc, char* argv[]) {
     if (ProcRank == 0)
         cout << "Parallel matrix-vector multiplication program\n";
 
-    // Memory allocation and data initialization
-    ProcessInitialization(pMatrix, pVector, pResult, Size);
-    cout << "Process " << ProcRank << " received matrix of size " << Size << "\n";
+    ProcessInitialization(pMatrix, pVector, pResult, pProcRows, pProcResult, Size, RowNum);
 
     MPI_Finalize();
 }
