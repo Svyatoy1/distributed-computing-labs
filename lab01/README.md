811b086600be5543fa9f536b89befc8bbbc99141
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-05 17:24:47 +0000

    gitignore test upd

---
+++ lab01/README.md
@@ -0,0 +1,2 @@
+### EX2 Task 1 - Open the Project (Стартове повідомлення)
+**Що зроблено:** після оголошення змінних додано вивід "Serial matrix-vector multiplication program", а також додано інформацію на gitignore
--- lab01/src/SerialMV.cpp
+++ lab01/src/SerialMV.cpp
@@ -3,3 +3,13 @@
 #include <ctime> // для функцій clock_t, clock, CLOCKS_PER_SEC
 #include <cstdlib>
 using namespace std;
+
+double* pMatrix; // First argument - initial matrix
+double* pVector; // Second argument - initial vector
+double* pResult; // Result vector for matrix-vector multiplication
+int Size; // Sizes of initial matrix and vector
+
+int main () {
+    cout << "Serial matrix-vector multiplication program" << '\n';
+    cin.get(); // крос-платформена заміна getch()
+}
