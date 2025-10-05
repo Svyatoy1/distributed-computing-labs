9393ecd7e35d6f0d354f9a5fba53c11fe473d0bf
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-05 21:32:33 +0000

    parallel program initialized

--- lab01/src/ParallelMV.cpp
+++ lab01/src/ParallelMV.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <mpi.h>
 using namespace std;
 
-int main (){
-    cout << "Parallel matrix-vectormultiplication program" << '\n';
-    return 0;
+void main(int argc, char* argv[]) {
+    double* pMatrix; // First argument - initial matrix
+    double* pVector; // Second argument - initial vector
+    double* pResult; // Result vector for matrix-vector multiplication
+    int Size; // Sizes of initial matrix and vector
+    double Start, Finish, Duration;
+    
+    MPI_Init(&argc, &argv);
+    printf (“Parallel matrix-vector multiplication program\n”)
+    MPI_Finalize();
 }
