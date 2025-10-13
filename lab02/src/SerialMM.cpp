18dd241bf84d39cd142e5754058095ff0b1b409b
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-13 06:47:45 +0000

    created functions to inputting the data into matrices and output them

--- lab02/src/SerialMM.cpp
+++ lab02/src/SerialMM.cpp
@@ -5,6 +5,27 @@
 #include <limits>
 using namespace std;
 
+// Function for simple initialization of matrix elements
+void DummyDataInitialization(double* pAMatrix, double* pBMatrix, int Size){
+    int i, j; // Loop variables
+    for (i=0; i<Size; i++) {
+        for (j=0; j<Size; j++) {
+            pAMatrix[i*Size+j] = 1;
+            pBMatrix[i*Size+j] = 1;
+        }
+    }
+}
+
+// Function for formatted matrix output
+void PrintMatrix (double* pMatrix, int RowCount, int ColCount) {
+    int i, j; // Loop variables
+    for (i=0; i<RowCount; i++) {
+        for (j=0; j<ColCount; j++)
+        printf("%7.4f ", pMatrix[i*ColCount+j]);
+        printf("\n");
+    }
+}
+
 // Function for memory allocation and initialization of matrix elements
 void ProcessInitialization (double* &pAMatrix, double* &pBMatrix, double* &pCMatrix, int &Size) {
     // Setting the size of matrices
@@ -16,6 +37,17 @@ void ProcessInitialization (double* &pAMatrix, double* &pBMatrix, double* &pCMat
             printf("\nSize of objects must be greater than 0!\n");
     }
     while (Size <= 0);
+
+    // Memory allocation
+    pAMatrix = new double [Size*Size];
+    pBMatrix = new double [Size*Size];
+    pCMatrix = new double [Size*Size];
+
+    // Initialization of matrix elements
+    DummyDataInitialization(pAMatrix, pBMatrix, Size);
+    for (int i=0; i<Size*Size; i++) {
+        pCMatrix[i] = 0;
+    }
 }
 
 int main(){
@@ -29,6 +61,12 @@ int main(){
 
     cout << "Serial matrix multiplication program\n";
     ProcessInitialization(pAMatrix, pBMatrix, pCMatrix, Size);
+
+    // Matrix output
+    printf ("\nInitial A Matrix \n");
+    PrintMatrix(pAMatrix, Size, Size);
+    printf("Initial B Matrix \n");
+    PrintMatrix(pBMatrix, Size, Size);
 
     return 0;
 }
