4a391dafad18074839de4ec3468a960673521d55
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-13 06:23:14 +0000

    created lab02 files: gitignore, readme file and SerialMM file

---
+++ lab02/src/SerialMM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime> // для функцій clock_t, clock, CLOCKS_PER_SEC
+#include <cstdlib>
+#include <limits>
+using namespace std;
+
+double* pAMatrix; // First argument of matrix multiplication
+double* pBMatrix; // Second argument of matrix multiplication
+double* pCMatrix; // Result matrix
+int Size; // Sizes of matrices
+
+int main(){
+    cout << "Serial matrix multiplication program\n";
+    return 0;
+}
