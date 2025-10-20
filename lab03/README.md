790d06094ff6e30a83507f150f97f3ee117940b1
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-20 06:54:39 +0000

    created README.md and ProcessInitalization function for memory allocation and data initialization

---
+++ lab03/README.md
@@ -0,0 +1,3 @@
+# EXERCISE 2 
+Запускаємо програму в наступній послідовності:
+`g++ src/SerialGauss.cpp -o SerialGauss` `./SerialGauss`
--- lab03/src/SerialGauss.cpp
+++ lab03/src/SerialGauss.cpp
@@ -5,12 +5,28 @@
 #include <limits>
 using namespace std;
 
+// Function for memory allocation and data initialization
+void ProcessInitialization (double* &pMatrix, double* &pVector, double* &pResult, int &Size) {
+    // Setting the size of the matrix and the vector
+    do {
+        printf("\nEnter the size of the matrix and the vector: ");
+        scanf("%d", &Size);
+        printf("\nChosen size = %d", Size);
+        if (Size <= 0)
+            printf("\nSize of objects must be greater than 0!\n");
+    } while (Size <= 0);
+}
+
 int main () {
     double* pMatrix; // Matrix of the linear system
     double* pVector; // Right parts of the linear system
     double* pResult; // Result vector
     int Size; // Size of the matrix and the vector
 
+    time_t start, finish;
+    double duration;
+
     printf("Serial Gauss algorithm for solving linear systems\n");
+    ProcessInitialization(pMatrix, pVector, pResult, Size);
     return 0;
 }
