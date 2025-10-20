aab5d14d7a39d4ba4265612034c751d79dc412cf
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-20 07:03:08 +0000

    created functions for Inputting the Initial Data (dummy data and random data) and outputting them

--- lab03/src/SerialGauss.cpp
+++ lab03/src/SerialGauss.cpp
@@ -5,6 +5,35 @@
 #include <limits>
 using namespace std;
 
+// Function for simple initialization of the matrix and the vector elements
+void DummyDataInitialization (double* pMatrix, double* pVector, int Size) {
+    int i, j; // Loop variables
+    for (i=0; i<Size; i++) {
+        pVector[i] = i+1;
+        for (j=0; j<Size; j++) {
+            if (j <= i)
+                pMatrix[i*Size+j] = 1;
+            else
+                pMatrix[i*Size+j] = 0;
+        }
+    }
+}
+
+// Function for random initialization of the matrix and the vector elements
+void RandomDataInitialization(double* pMatrix, double* pVector, int Size) {
+    int i, j; // Loop variables
+    srand(unsigned(clock()));
+    for (i=0; i<Size; i++) {
+        pVector[i] = rand()/double(1000);
+        for (j=0; j<Size; j++) {
+            if (j <= i)
+                pMatrix[i*Size+j] = rand()/double(1000);
+            else
+                pMatrix[i*Size+j] = 0;
+        }
+    }
+}
+
 // Function for memory allocation and data initialization
 void ProcessInitialization (double* &pMatrix, double* &pVector, double* &pResult, int &Size) {
     // Setting the size of the matrix and the vector
@@ -15,6 +44,32 @@ void ProcessInitialization (double* &pMatrix, double* &pVector, double* &pResult
         if (Size <= 0)
             printf("\nSize of objects must be greater than 0!\n");
     } while (Size <= 0);
+
+    // Memory allocation
+    pMatrix = new double [Size*Size];
+    pVector = new double [Size];
+    pResult = new double [Size];
+
+    // Initialization of the matrix and the vector elements
+    DummyDataInitialization(pMatrix, pVector, Size);
+}
+
+// Function for formatted matrix output
+void PrintMatrix (double* pMatrix, int RowCount, int ColCount) {
+    int i, j; // Loop variables
+        for (i=0; i<RowCount; i++) {
+            for (j=0; j<ColCount; j++)
+            printf("%7.4f ", pMatrix[i*ColCount+j]);
+            printf("\n");
+        }
+    }
+    
+// Function for formatted vector output
+void PrintVector (double* pVector, int Size) {
+    int i;
+    for (i=0; i<Size; i++)
+    printf("%7.4f ", pVector[i]);
+    printf("\n");
 }
 
 int main () {
@@ -28,5 +83,12 @@ int main () {
 
     printf("Serial Gauss algorithm for solving linear systems\n");
     ProcessInitialization(pMatrix, pVector, pResult, Size);
+
+    // Matrix and vector output
+    printf ("\nInitial Matrix \n");
+    PrintMatrix(pMatrix, Size, Size);
+    printf("Initial Vector \n");
+    PrintVector(pVector, Size);
+
     return 0;
 }
