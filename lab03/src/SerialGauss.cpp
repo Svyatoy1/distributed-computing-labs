5ee20de26166255caa4ff78394f4dbc5749ff2fe
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-20 06:48:05 +0000

    created SerialGauss.cpp and tested in Terminal

---
+++ lab03/src/SerialGauss.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime> // для функцій clock_t, clock, CLOCKS_PER_SEC
+#include <cstdlib>
+#include <limits>
+using namespace std;
+
+int main () {
+    double* pMatrix; // Matrix of the linear system
+    double* pVector; // Right parts of the linear system
+    double* pResult; // Result vector
+    int Size; // Size of the matrix and the vector
+
+    printf("Serial Gauss algorithm for solving linear systems\n");
+    return 0;
+}
