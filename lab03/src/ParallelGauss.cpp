17dd28e207bdd145f02f8cd2309b1d1b08739b16
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-24 18:42:12 +0000

    creating TestResult function the Parallel Program Correctness

--- lab03/src/ParallelGauss.cpp
+++ lab03/src/ParallelGauss.cpp
@@ -366,6 +366,34 @@ void PrintResultVector (double* pResult, int Size) {
         printf("%7.4f ", pResult[pParallelPivotPos[i]]);
 }
 
+// Function for testing the result
+void TestResult(double* pMatrix, double* pVector, double* pResult, int Size) {
+    /* Buffer for storing the vector, that is a result of multiplication
+    of the linear system matrix by the vector of unknowns */
+    double* pRightPartVector;
+    // Flag, that shows wheather the right parts vectors are identical or not
+    int equal = 0;
+    double Accuracy = 1.e-6; // Comparison accuracy
+    if (ProcRank == 0) {
+        pRightPartVector = new double [Size];
+        for (int i=0; i<Size; i++) {
+            pRightPartVector[i] = 0;
+            for (int j=0; j<Size; j++) {
+                pRightPartVector[i] += pMatrix[i*Size+j]*pResult[pParallelPivotPos[j]];
+            }
+        }
+        for (int i=0; i<Size; i++) {
+            if (fabs(pRightPartVector[i]-pVector[i]) > Accuracy)
+                equal = 1;
+        }
+        if (equal == 1)
+            printf("The result of the parallel Gauss algorithm is NOT correct. Check your code.");
+        else
+            printf("The result of the parallel Gauss algorithm is correct.");
+        delete [] pRightPartVector;
+    }
+}
+
 // Function for computational process termination
 void ProcessTermination (double* pMatrix, double* pVector, double* pResult,
 double* pProcRows, double* pProcVector, double* pProcResult) {
