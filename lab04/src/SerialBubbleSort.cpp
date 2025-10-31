4ff23b27a2d9ef28ccd986437cfa8da149b2220a
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-10-31 10:15:55 +0000

    Inputting the Sorted Data Size (created datainitialization and output functions)

--- lab04/src/SerialBubbleSort.cpp
+++ lab04/src/SerialBubbleSort.cpp
@@ -9,6 +9,39 @@ using namespace std;
 double *pData; // Data to be sorted
 int DataSize; // Size of data to be sorted
 
+// Function for simple setting the initial data
+void DummyDataInitialization(double*& pData, int& DataSize) {
+    for(int i = 0; i < DataSize; i++)
+        pData[i] = DataSize - i;
+}
+
+// Function for formatted data output
+void PrintData(double *pData, int DataSize) {
+    for(int i = 0; i < DataSize; i++)
+        printf("%7.4f ", pData[i]);
+    printf("\n");
+}
+
+// Function for allocating the memory and setting the initial values
+void ProcessInitialization(double *&pData, int& DataSize) {
+    do {
+        printf("Enter the size of data to be sorted: ");
+        scanf("%d", &DataSize);
+        if(DataSize <= 0)
+            printf("Data size should be greater than zero\n");
+    } while(DataSize <= 0);
+    printf("Sorting %d data items\n", DataSize);
+
+    pData = new double[DataSize];
+    DummyDataInitialization(pData, DataSize);
+}
+
 int main() {
-    cout << "Serial bubble sort program \n";
+    printf("Serial bubble sort program\n");
+    // Process initialization
+    ProcessInitialization(pData, DataSize);
+
+    printf("Data before sorting\n");
+    PrintData(pData, DataSize);
+
 }
