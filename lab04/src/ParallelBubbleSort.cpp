eb52fc1008d5b4ea757d88d14d3efa8142b82f49
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-11-02 19:30:25 +0000

    creatinf function for Inputting the Initial Data

--- lab04/src/ParallelBubbleSort.cpp
+++ lab04/src/ParallelBubbleSort.cpp
@@ -23,6 +23,35 @@ void RandomDataInitialization(double *&pData, int& DataSize) {
     pData[i] = double(rand()) / RAND_MAX * RandomDataMultiplier;
 }
 
+// Function for allocating the memory and setting the initial values
+void ProcessInitialization(double *&pData, int& DataSize, double *&pProcData, int& BlockSize) {
+    setvbuf(stdout, 0, _IONBF, 0);
+    if(ProcRank == 0) {
+        do {
+            printf("Enter the size of data to be sorted: ");
+            scanf("%d", &DataSize);
+            if(DataSize < ProcNum) {
+                printf("Data size should be greater than number of processes\n");
+            }
+            if(DataSize % ProcNum != 0) {
+                printf("Data size should be divisible by number of processes\n");
+            }
+        } while((DataSize < ProcNum) || (DataSize % ProcNum != 0));
+        printf("Sorting %d data items\n", DataSize);
+    }
+    // Broadcasting the data size
+    MPI_Bcast(&DataSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    BlockSize = DataSize / ProcNum;
+
+    pProcData = new double[BlockSize];
+    if (ProcRank == 0) {
+        pData = new double[DataSize];
+        
+    // Data initalization
+    DummyDataInitialization(pData, DataSize);
+    }
+}
+
 // Function for formatted data output
 void PrintData(double *pData, int DataSize) {
     for(int i = 0; i < DataSize; i++)
@@ -35,11 +64,17 @@ int main (int argc, char* argv[]) {
     double *pProcData = 0;
     int DataSize = 0;
     int BlockSize = 0;
-    
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-    printf("Parallel bubble sort program\n");
+
+    if (ProcRank == 0)
+        printf("Parallel bubble sort program\n");
+
+    // Process initialization
+    ProcessInitialization(pData, DataSize, pProcData, BlockSize);
+
     MPI_Finalize();
     return 0;
 }
