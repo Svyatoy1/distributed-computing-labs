208b9e28e0587992e0237ca3ce6327c8867f7f80
Svyatoy1 <svyatoslavpershko@gmail.com>
2025-11-14 16:35:07 +0000

    created parallel program

---
+++ lab05/src/ParallelFloyd.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+#include <algorithm>
+#include <mpi.h>
+
+int main () {
+    printf("Parallel Floyd algorithm\n");
+    return 0;
+}
